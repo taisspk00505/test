4f81633b0890d4142b38bb95e92bccbcc8036260
taisspk00505 <taisspk00505@gmail.com>
2016-03-08 10:11:22 +0700

    Added files via upload

---
+++ hienbqpk00475+taipk00505_Assignment/WindowsApplication1/FrmQLSV.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmQLSV
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LbMSV = New System.Windows.Forms.Label()
+        Me.LbTSV = New System.Windows.Forms.Label()
+        Me.LbGT = New System.Windows.Forms.Label()
+        Me.LbNS = New System.Windows.Forms.Label()
+        Me.LbDC = New System.Windows.Forms.Label()
+        Me.LbSDT = New System.Windows.Forms.Label()
+        Me.TxbMSV = New System.Windows.Forms.TextBox()
+        Me.TxbTSV = New System.Windows.Forms.TextBox()
+        Me.TxbNS = New System.Windows.Forms.TextBox()
+        Me.TxbDC = New System.Windows.Forms.TextBox()
+        Me.TxbSDT = New System.Windows.Forms.TextBox()
+        Me.BtThem = New System.Windows.Forms.Button()
+        Me.BtSua = New System.Windows.Forms.Button()
+        Me.BtXoa = New System.Windows.Forms.Button()
+        Me.BtThoat = New System.Windows.Forms.Button()
+        Me.LwQLSV = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.CbGT = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'LbMSV
+        '
+        Me.LbMSV.AutoSize = True
+        Me.LbMSV.Location = New System.Drawing.Point(80, 41)
+        Me.LbMSV.Name = "LbMSV"
+        Me.LbMSV.Size = New System.Drawing.Size(87, 17)
+        Me.LbMSV.TabIndex = 0
+        Me.LbMSV.Text = "Mã sinh viên"
+        '
+        'LbTSV
+        '
+        Me.LbTSV.AutoSize = True
+        Me.LbTSV.Location = New System.Drawing.Point(80, 83)
+        Me.LbTSV.Name = "LbTSV"
+        Me.LbTSV.Size = New System.Drawing.Size(93, 17)
+        Me.LbTSV.TabIndex = 0
+        Me.LbTSV.Text = "Tên sinh viên"
+        '
+        'LbGT
+        '
+        Me.LbGT.AutoSize = True
+        Me.LbGT.Location = New System.Drawing.Point(80, 132)
+        Me.LbGT.Name = "LbGT"
+        Me.LbGT.Size = New System.Drawing.Size(60, 17)
+        Me.LbGT.TabIndex = 0
+        Me.LbGT.Text = "Giới tính"
+        '
+        'LbNS
+        '
+        Me.LbNS.AutoSize = True
+        Me.LbNS.Location = New System.Drawing.Point(594, 39)
+        Me.LbNS.Name = "LbNS"
+        Me.LbNS.Size = New System.Drawing.Size(71, 17)
+        Me.LbNS.TabIndex = 0
+        Me.LbNS.Text = "Ngày sinh"
+        '
+        'LbDC
+        '
+        Me.LbDC.AutoSize = True
+        Me.LbDC.Location = New System.Drawing.Point(594, 83)
+        Me.LbDC.Name = "LbDC"
+        Me.LbDC.Size = New System.Drawing.Size(51, 17)
+        Me.LbDC.TabIndex = 0
+        Me.LbDC.Text = "Địa chỉ"
+        '
+        'LbSDT
+        '
+        Me.LbSDT.AutoSize = True
+        Me.LbSDT.Location = New System.Drawing.Point(594, 132)
+        Me.LbSDT.Name = "LbSDT"
+        Me.LbSDT.Size = New System.Drawing.Size(91, 17)
+        Me.LbSDT.TabIndex = 0
+        Me.LbSDT.Text = "Số điện thoại"
+        '
+        'TxbMSV
+        '
+        Me.TxbMSV.Location = New System.Drawing.Point(209, 36)
+        Me.TxbMSV.Name = "TxbMSV"
+        Me.TxbMSV.Size = New System.Drawing.Size(260, 22)
+        Me.TxbMSV.TabIndex = 1
+        '
+        'TxbTSV
+        '
+        Me.TxbTSV.Location = New System.Drawing.Point(209, 83)
+        Me.TxbTSV.Name = "TxbTSV"
+        Me.TxbTSV.Size = New System.Drawing.Size(260, 22)
+        Me.TxbTSV.TabIndex = 1
+        '
+        'TxbNS
+        '
+        Me.TxbNS.Location = New System.Drawing.Point(720, 36)
+        Me.TxbNS.Name = "TxbNS"
+        Me.TxbNS.Size = New System.Drawing.Size(260, 22)
+        Me.TxbNS.TabIndex = 1
+        '
+        'TxbDC
+        '
+        Me.TxbDC.Location = New System.Drawing.Point(720, 83)
+        Me.TxbDC.Name = "TxbDC"
+        Me.TxbDC.Size = New System.Drawing.Size(260, 22)
+        Me.TxbDC.TabIndex = 1
+        '
+        'TxbSDT
+        '
+        Me.TxbSDT.Location = New System.Drawing.Point(720, 127)
+        Me.TxbSDT.Name = "TxbSDT"
+        Me.TxbSDT.Size = New System.Drawing.Size(260, 22)
+        Me.TxbSDT.TabIndex = 1
+        '
+        'BtThem
+        '
+        Me.BtThem.Location = New System.Drawing.Point(209, 206)
+        Me.BtThem.Name = "BtThem"
+        Me.BtThem.Size = New System.Drawing.Size(75, 23)
+        Me.BtThem.TabIndex = 2
+        Me.BtThem.Text = "Thêm"
+        Me.BtThem.UseVisualStyleBackColor = True
+        '
+        'BtSua
+        '
+        Me.BtSua.Location = New System.Drawing.Point(349, 206)
+        Me.BtSua.Name = "BtSua"
+        Me.BtSua.Size = New System.Drawing.Size(75, 23)
+        Me.BtSua.TabIndex = 2
+        Me.BtSua.Text = "Sửa"
+        Me.BtSua.UseVisualStyleBackColor = True
+        '
+        'BtXoa
+        '
+        Me.BtXoa.Location = New System.Drawing.Point(489, 206)
+        Me.BtXoa.Name = "BtXoa"
+        Me.BtXoa.Size = New System.Drawing.Size(75, 23)
+        Me.BtXoa.TabIndex = 2
+        Me.BtXoa.Text = "Xóa"
+        Me.BtXoa.UseVisualStyleBackColor = True
+        '
+        'BtThoat
+        '
+        Me.BtThoat.Location = New System.Drawing.Point(625, 206)
+        Me.BtThoat.Name = "BtThoat"
+        Me.BtThoat.Size = New System.Drawing.Size(75, 23)
+        Me.BtThoat.TabIndex = 2
+        Me.BtThoat.Text = "Thoát"
+        Me.BtThoat.UseVisualStyleBackColor = True
+        '
+        'LwQLSV
+        '
+        Me.LwQLSV.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6, Me.ColumnHeader7})
+        Me.LwQLSV.FullRowSelect = True
+        Me.LwQLSV.GridLines = True
+        Me.LwQLSV.Location = New System.Drawing.Point(38, 289)
+        Me.LwQLSV.Name = "LwQLSV"
+        Me.LwQLSV.Size = New System.Drawing.Size(1007, 356)
+        Me.LwQLSV.TabIndex = 3
+        Me.LwQLSV.UseCompatibleStateImageBehavior = False
+        Me.LwQLSV.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "STT"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Mã sinh viên"
+        Me.ColumnHeader2.Width = 124
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Tên sinh viên"
+        Me.ColumnHeader3.Width = 209
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Gới tính"
+        Me.ColumnHeader4.Width = 124
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Ngày sinh"
+        Me.ColumnHeader5.Width = 137
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Địa chỉ"
+        Me.ColumnHeader6.Width = 170
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "Số điện thoại"
+        Me.ColumnHeader7.Width = 176
+        '
+        'CbGT
+        '
+        Me.CbGT.FormattingEnabled = True
+        Me.CbGT.Items.AddRange(New Object() {"True", "False"})
+        Me.CbGT.Location = New System.Drawing.Point(209, 129)
+        Me.CbGT.Name = "CbGT"
+        Me.CbGT.Size = New System.Drawing.Size(260, 24)
+        Me.CbGT.TabIndex = 4
+        '
+        'FrmQLSV
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1080, 674)
+        Me.Controls.Add(Me.CbGT)
+        Me.Controls.Add(Me.LwQLSV)
+        Me.Controls.Add(Me.BtThoat)
+        Me.Controls.Add(Me.BtXoa)
+        Me.Controls.Add(Me.BtSua)
+        Me.Controls.Add(Me.BtThem)
+        Me.Controls.Add(Me.TxbSDT)
+        Me.Controls.Add(Me.TxbDC)
+        Me.Controls.Add(Me.TxbNS)
+        Me.Controls.Add(Me.TxbTSV)
+        Me.Controls.Add(Me.TxbMSV)
+        Me.Controls.Add(Me.LbSDT)
+        Me.Controls.Add(Me.LbDC)
+        Me.Controls.Add(Me.LbNS)
+        Me.Controls.Add(Me.LbGT)
+        Me.Controls.Add(Me.LbTSV)
+        Me.Controls.Add(Me.LbMSV)
+        Me.Name = "FrmQLSV"
+        Me.Text = "QLSinhVien"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LbMSV As System.Windows.Forms.Label
+    Friend WithEvents LbTSV As System.Windows.Forms.Label
+    Friend WithEvents LbGT As System.Windows.Forms.Label
+    Friend WithEvents LbNS As System.Windows.Forms.Label
+    Friend WithEvents LbDC As System.Windows.Forms.Label
+    Friend WithEvents LbSDT As System.Windows.Forms.Label
+    Friend WithEvents TxbMSV As System.Windows.Forms.TextBox
+    Friend WithEvents TxbTSV As System.Windows.Forms.TextBox
+    Friend WithEvents TxbNS As System.Windows.Forms.TextBox
+    Friend WithEvents TxbDC As System.Windows.Forms.TextBox
+    Friend WithEvents TxbSDT As System.Windows.Forms.TextBox
+    Friend WithEvents BtThem As System.Windows.Forms.Button
+    Friend WithEvents BtSua As System.Windows.Forms.Button
+    Friend WithEvents BtXoa As System.Windows.Forms.Button
+    Friend WithEvents BtThoat As System.Windows.Forms.Button
+    Friend WithEvents LwQLSV As System.Windows.Forms.ListView
+    Friend WithEvents CbGT As System.Windows.Forms.ComboBox
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader7 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ hienbqpk00475+taipk00505_Assignment/WindowsApplication1/FrmQLSV.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class FrmQLSV
+    Dim sqlConnect As New QLSinhVien
+    Private Sub FrmQLSV_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadQLSV()
+    End Sub
+
+    Private Sub LoadQLSV()
+        LwQLSV.Items.Clear()
+        Dim dt As DataTable = sqlConnect.GetDataTable("Select * from DanhSach")
+
+        For i As Integer = 0 To dt.Rows.Count - 1 Step 1
+            Dim item As New ListViewItem(LwQLSV.Items.Count + 1)
+            item.SubItems.Add(dt.Rows(i).ItemArray(1))
+            item.SubItems.Add(dt.Rows(i).ItemArray(2))
+            item.SubItems.Add(dt.Rows(i).ItemArray(3))
+            item.SubItems.Add(dt.Rows(i).ItemArray(4))
+            item.SubItems.Add(dt.Rows(i).ItemArray(5))
+            item.SubItems.Add(dt.Rows(i).ItemArray(6))
+            LwQLSV.Items.Add(item)
+        Next
+
+    End Sub
+    Private Sub BtThem_Click(sender As Object, e As EventArgs) Handles BtThem.Click
+        If TxbMSV.Text = "" Then
+            MessageBox.Show("Vui lòng nhập mã sinh viên")
+        ElseIf TxbTSV.Text = "" Then
+            MessageBox.Show("Vui lòng nhập tên sinh viên")
+        ElseIf CbGT.Text = "" Then
+            MessageBox.Show("Vui lòng chọn giới tính")
+        ElseIf TxbNS.Text = "" Then
+            MessageBox.Show("Vui lòng nhập ngày sinh")
+        ElseIf TxbDC.Text = "" Then
+            MessageBox.Show("Vui lòng nhập địa chỉ")
+        ElseIf TxbSDT.Text = "" Then
+            MessageBox.Show("Vui lòng nhập số điện thoại")
+        Else
+            sqlConnect.ExecuteNoneQuery("Insert into DanhSach(MaSV,TenSV,GioiTinh,NgaySinh,DiaChi,SDT) values('" + TxbMSV.Text + "',N'" + TxbTSV.Text + "','" + CbGT.Text + "','" + TxbNS.Text + "',N'" + TxbDC.Text + "','" + TxbSDT.Text + "')")
+            LoadQLSV()
+        End If
+    End Sub
+
+    Private Sub BtThoat_Click(sender As Object, e As EventArgs) Handles BtThoat.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub BtSua_Click(sender As Object, e As EventArgs) Handles BtSua.Click
+        If TxbMSV.Text = "" Then
+            MessageBox.Show("Vui lòng nhập mã sinh viên")
+        ElseIf TxbTSV.Text = "" Then
+            MessageBox.Show("Vui lòng nhập tên sinh viên")
+        ElseIf CbGT.Text = "" Then
+            MessageBox.Show("Vui lòng chọn giới tính")
+        ElseIf TxbNS.Text = "" Then
+            MessageBox.Show("Vui lòng nhập ngày sinh")
+        ElseIf TxbDC.Text = "" Then
+            MessageBox.Show("Vui lòng nhập địa chỉ")
+        ElseIf TxbSDT.Text = "" Then
+            MessageBox.Show("Vui lòng nhập số điện thoại")
+        Else
+            For i As Integer = LwQLSV.SelectedIndices.Count - 1 To 0 Step -1
+                Dim viTriCanSua As Integer = LwQLSV.SelectedIndices(i)
+                Dim maSvCanSua As String = LwQLSV.Items(viTriCanSua).SubItems(1).Text
+                sqlConnect.ExecuteNoneQuery("Update DanhSach set MaSV='" + TxbMSV.Text + "',TenSV=N'" + TxbTSV.Text + "',GioiTinh='" + CbGT.Text + "',NgaySinh='" + TxbNS.Text + "',DiaChi=N'" + TxbDC.Text + "',SDT='" + TxbSDT.Text + "' where MaSV=" + maSvCanSua)
+            Next
+            LoadQLSV()
+        End If
+    End Sub
+
+    Private Sub BtXoa_Click(sender As Object, e As EventArgs) Handles BtXoa.Click
+        For i As Integer = LwQLSV.SelectedIndices.Count - 1 To 0 Step -1
+            Dim viTriCanXoa As Integer = LwQLSV.SelectedIndices(i)
+            Dim maSvCanXoa As String = LwQLSV.Items(viTriCanXoa).SubItems(1).Text
+            sqlConnect.ExecuteNoneQuery("Delete from DanhSach where MaSV =" + maSvCanXoa)
+        Next
+        LoadQLSV()
+    End Sub
+
+    Private Sub LwQLSV_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LwQLSV.SelectedIndexChanged
+        For Each ds As ListViewItem In LwQLSV.SelectedItems
+            TxbMSV.Text = ds.SubItems(1).Text
+            TxbTSV.Text = ds.SubItems(2).Text
+            CbGT.Text = ds.SubItems(3).Text
+            TxbNS.Text = ds.SubItems(4).Text
+            TxbDC.Text = ds.SubItems(5).Text
+            TxbSDT.Text = ds.SubItems(6).Text
+        Next
+    End Sub
+End Class
